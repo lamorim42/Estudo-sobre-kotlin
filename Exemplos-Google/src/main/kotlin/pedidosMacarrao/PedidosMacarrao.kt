d18d30f4541e95a5e60ee7051826dadae395b514
Luís Amorim <luishads@gmail.com>
2021-04-02 20:28:21 -0300

    Primeiro exemplo, que não é o primeiro na ordem

--- Exemplos-Google/src/main/kotlin/pedidosMacarrao/PedidosMacarrao.kt
+++ Exemplos-Google/src/main/kotlin/pedidosMacarrao/PedidosMacarrao.kt
@@ -1,3 +1,5 @@
+package pedidosMacarrao
+
 open class Item(val name: String, val price: Int)
 
 class Noodles : Item("Noodles", 10) {
@@ -16,15 +18,17 @@ class Vegetables(private vararg val toppings: String) : Item("Vegetables", 5) {
     }
 }
 
-class Order (val orderNumber: Int) {
+class Order (private val orderNumber: Int) {
     private val itemList = mutableListOf<Item>()
 
-    fun addItem(newItem: Item) {
+    fun addItem(newItem: Item): Order {
         itemList.add(newItem)
+        return this
     }
 
-    fun addAll(newItems: List<Item>) {
+    fun addAll(newItems: List<Item>): Order {
         itemList.addAll(newItems)
+        return this
     }
 
     fun print() {
@@ -39,21 +43,31 @@ class Order (val orderNumber: Int) {
 }
 
 fun main() {
+    val ordersList = mutableListOf<Order>()
+
     val order1 = Order(1)
     order1.addItem(Noodles())
-    order1.print()
+    ordersList.add(order1)
 
-    println()
 
-    val order2 = Order(2)
-    order2.addItem(Noodles())
-    order2.addItem(Vegetables())
-    order2.print()
+    val order2 = Order(2).addItem(Noodles()).addItem(Vegetables())
+    ordersList.add(order2)
 
-    println()
-
-    val order3 = Order(3)
     val items = listOf(Noodles(), Vegetables("Carrots", "Beans", "Celery"))
-    order3.addAll(items)
-    order3.print()
+    val order3 = Order(3).addAll(items)
+    ordersList.add(order3)
+
+    val order4 = Order(4).addItem(Noodles()).addItem(Vegetables("Cabbage", "Onion"))
+    ordersList.add(order4)
+
+    ordersList.add(
+        Order(5)
+            .addItem(Noodles())
+            .addItem(Noodles())
+            .addItem(Vegetables("Spinach")))
+
+    for (order in ordersList) {
+        order.print()
+        println()
+    }
 }
